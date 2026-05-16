16d7a2225256699fa37155bf7c99d62db6ed135a
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-16 16:39:17 +0200

    fixes soft update all

--- softac/functions/soft_update_all.py
+++ softac/functions/soft_update_all.py
@@ -1,13 +1,10 @@
 import torch
-from itertools import chain
 from typing import List
+from .soft_update import soft_update
 
 
 def soft_update_all(
     *, sources: List[torch.nn.Module], targets: List[torch.nn.Module], tau: float
 ) -> None:
-    for source_parameter, target_parameter in chain(
-        *(source.parameters() for source in sources),
-        *(target.parameters() for target in targets),
-    ):
-        target_parameter.data.mul_(1 - tau).add_(source_parameter.data, alpha=tau)
+    for source, target in zip(sources, targets):
+        soft_update(source=source, target=target, tau=tau)
